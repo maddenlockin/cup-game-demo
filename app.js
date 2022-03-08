76399b8e9d35487d2140a8030b5b132db5e182a0
maddenlockin <maddenlockin@gmail.com>
2022-03-07 17:16:09 -0800

    grabbed DOM elements

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
-
+const disImage0 = document.getElementById('cup-0');
+const disImage1 = document.getElementById('cup-1');
+const disImage2 = document.getElementById('cup-2');
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
+const button0 = document.getElementById('button-0');
+const button1 = document.getElementById('button-1');
+const button2 = document.getElementById('button-2');
 // let state
 
 // set event listeners 
