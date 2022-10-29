ea15719dc4bfd3db6ff42799aab8f530ea75c958
maddenlockin <maddenlockin@gmail.com>
2022-10-29 16:10:12 -0700

    clear for new demo and add css

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
 // import functions and grab DOM elements
-const disImage0 = document.getElementById('cup-0');
-const disImage1 = document.getElementById('cup-1');
-const disImage2 = document.getElementById('cup-2');
-const winsEl = document.getElementById('wins');
-const lossesEl = document.getElementById('losses');
-const totalEl = document.getElementById('total');
-const button0 = document.getElementById('button-0');
-const button1 = document.getElementById('button-1');
-const button2 = document.getElementById('button-2');
+
 // let state
-let wins = 0;
-let total = 0;
-
-// create handler function for user guess
-  // set display image src
-  // increment total 
-  // randomize app cup choice & compare
-  // update display image 
-  // update win/loss/total
-
-// set event listeners 
-  // event for each button, call handler function 
-
