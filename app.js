8d66928164e8efd586d66e8538d289f1b7737607
maddenlockin <maddenlockin@gmail.com>
2022-12-09 09:59:51 -0800

    buttons 2 and 3 working

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const img2 = document.querySelector('#img-2');
 const img3 = document.querySelector('#img-3');
 
 const button1 = document.getElementById('button-1');
+const button2 = document.getElementById('button-2');
+const button3 = document.getElementById('button-3');
 
 const winsEl = document.getElementById('wins');
 const lossesEl = document.getElementById('losses');
@@ -15,6 +17,40 @@ let total = 0;
 
 //button click event listener
 button1.addEventListener('click', () => {
+    total++;
+    resetShells();
+    //generate random location (number between 1 and 3)
+    const pearlLocation = Math.ceil(Math.random() * 3);
+    //take that location and add the reveal class to it (if.. else)
+    if (pearlLocation === 1) {
+        wins++;
+        img1.classList.add('reveal');
+    } else if (pearlLocation === 2) {
+        img2.classList.add('reveal');
+    } else {
+        img3.classList.add('reveal');
+    }
+    displayResults();
+});
+
+button2.addEventListener('click', () => {
+    total++;
+    resetShells();
+    //generate random location (number between 1 and 3)
+    const pearlLocation = Math.ceil(Math.random() * 3);
+    //take that location and add the reveal class to it (if.. else)
+    if (pearlLocation === 1) {
+        wins++;
+        img1.classList.add('reveal');
+    } else if (pearlLocation === 2) {
+        img2.classList.add('reveal');
+    } else {
+        img3.classList.add('reveal');
+    }
+    displayResults();
+});
+
+button3.addEventListener('click', () => {
     total++;
     resetShells();
     //generate random location (number between 1 and 3)
